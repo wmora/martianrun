88d4033b46557b0599e6871d883cd8c7abf17e50
William Mora <wilmor24@gmail.com>
2014-06-01 23:07:47 -0300

    Ground asset

--- core/src/com/gamestudio24/cityescape/utils/Constants.java
+++ core/src/com/gamestudio24/cityescape/utils/Constants.java
@@ -6,12 +6,13 @@ public class Constants {
 
     public static final int APP_WIDTH = 800;
     public static final int APP_HEIGHT = 480;
+    public static final float WORLD_TO_SCREEN = 32;
 
     public static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
 
     public static final float GROUND_X = 0;
     public static final float GROUND_Y = 0;
-    public static final float GROUND_WIDTH = 50f;
+    public static final float GROUND_WIDTH = 25f;
     public static final float GROUND_HEIGHT = 2f;
     public static final float GROUND_DENSITY = 0f;
 
@@ -34,4 +35,5 @@ public class Constants {
     public static final Vector2 ENEMY_LINEAR_VELOCITY = new Vector2(-10f, 0);
 
     public static final String BACKGROUND_IMAGE_PATH = "background.png";
+    public static final String GROUND_IMAGE_PATH = "ground.png";
 }
