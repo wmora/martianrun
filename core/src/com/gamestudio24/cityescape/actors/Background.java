d46b2dee46f319258478b9c239ea2a0e0afb47d1
William Mora <wilmor24@gmail.com>
2014-06-02 01:43:32 -0300

    Refactor rendering the background

--- core/src/com/gamestudio24/cityescape/actors/Background.java
+++ core/src/com/gamestudio24/cityescape/actors/Background.java
@@ -10,15 +10,13 @@ import com.gamestudio24.cityescape.utils.Constants;
 
 public class Background extends Actor {
 
-    private final TextureRegion textureRegion1;
-    private final TextureRegion textureRegion2;
+    private final TextureRegion textureRegion;
     private Rectangle textureRegionBounds1;
     private Rectangle textureRegionBounds2;
     private int speed = 100;
 
     public Background() {
-        textureRegion1 = new TextureRegion(new Texture(Gdx.files.internal(Constants.BACKGROUND_IMAGE_PATH)));
-        textureRegion2 = new TextureRegion(new Texture(Gdx.files.internal(Constants.BACKGROUND_IMAGE_PATH)));
+        textureRegion = new TextureRegion(new Texture(Gdx.files.internal(Constants.BACKGROUND_IMAGE_PATH)));
         textureRegionBounds1 = new Rectangle(0 - Constants.APP_WIDTH / 2, 0, Constants.APP_WIDTH, Constants.APP_HEIGHT);
         textureRegionBounds2 = new Rectangle(Constants.APP_WIDTH / 2, 0, Constants.APP_WIDTH, Constants.APP_HEIGHT);
     }
@@ -35,9 +33,9 @@ public class Background extends Actor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        batch.draw(textureRegion1, textureRegionBounds1.x, textureRegionBounds1.y, Constants.APP_WIDTH,
+        batch.draw(textureRegion, textureRegionBounds1.x, textureRegionBounds1.y, Constants.APP_WIDTH,
                 Constants.APP_HEIGHT);
-        batch.draw(textureRegion2, textureRegionBounds2.x, textureRegionBounds2.y, Constants.APP_WIDTH,
+        batch.draw(textureRegion, textureRegionBounds2.x, textureRegionBounds2.y, Constants.APP_WIDTH,
                 Constants.APP_HEIGHT);
     }
 
