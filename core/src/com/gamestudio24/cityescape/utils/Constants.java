7f2af319d127e2d32260fcc6eb6a14d9e91eec49
William Mora <wilmor24@gmail.com>
2014-06-06 22:47:12 -0300

    New font

--- core/src/com/gamestudio24/cityescape/utils/Constants.java
+++ core/src/com/gamestudio24/cityescape/utils/Constants.java
@@ -62,5 +62,5 @@ public class Constants {
     public static final String RUNNER_HIT_SOUND = "hit.wav";
     public static final String GAME_MUSIC = "fun_in_a_bottle.mp3";
 
-    public static final String FONT_NAME = "opensans_bold.ttf";
+    public static final String FONT_NAME = "roboto_bold.ttf";
 }
